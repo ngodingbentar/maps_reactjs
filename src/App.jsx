4ec2445772af55988c2455249d41e0178e8258ae
ngodingbentar <dewaqintoro@gmail.com>
2022-09-25 21:46:23 +0700

    render items on map

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ function App() {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import useStyles from './Styles'
 import GoogleMapReact from 'google-map-react'
-import { useMediaQuery } from '@material-ui/core'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-function Map({ setCoordinates, setBounds, coordinates }) {
+function Map({ setCoordinates, setBounds, coordinates, places }) {
   const isMobile = useMediaQuery('(min-width:600px)')
   const classes = useStyles()
+  const cek = () => {
+    console.log('places', places)
+  }
   // const coordinates = { lat: 0, lng: 0 }
   return (
     <div className={classes.mapContainer}>
+      <div><button onClick={() => cek()}>cek 1</button></div>
       <GoogleMapReact
         bootstrapURLKeys={{key: ''}}
         defaultCenter={coordinates}
@@ -21,7 +27,30 @@ function Map({ setCoordinates, setBounds, coordinates }) {
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
         }}
         onChildClick={''}
-      ></GoogleMapReact>
+      >
+        {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isMobile
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                    alt={place.name}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   )
 }
