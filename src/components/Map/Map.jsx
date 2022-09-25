1ad6349ded790e33412b4f22951980ede22c1b3f
ngodingbentar <dewaqintoro@gmail.com>
2022-09-25 18:00:49 +0700

    added: map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import useStyles from './Styles'
+import GoogleMapReact from 'google-map-react'
+import { useMediaQuery } from '@material-ui/core'
 
 function Map() {
+  const isMobile = useMediaQuery('(min-width:600px)')
+  const classes = useStyles()
+  const coordinates = { lat: 0, lng: 0 }
+
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{key: ''}}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
   )
 }
 
